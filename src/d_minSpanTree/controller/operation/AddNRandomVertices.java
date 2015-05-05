d563508e5f76f8475a5ac9d4f79c091f46084ced
Dalen W. Brauner <Dalen.Brauner12@ncf.edu>
2015-05-04 23:50:32 -0400

    Added basic pointInsideCircumcircle()! Un-commented delanay code!
    Additional Changes:
    * Fixed height's hard-coded value (to match the hard-coded windowHeight)
    * superTriangle now encompasses entire space (plus some extra)
      (superTriangle is now a right triangle, with its right angle 0.1 above and 0.1 to the left of (0,0))
      (previously, vertices in the bottom-right corner could have been excluded)
      (previously, vertices on the very edges could have been excluded)

--- src/d_minSpanTree/controller/operation/AddNRandomVertices.java
+++ src/d_minSpanTree/controller/operation/AddNRandomVertices.java
@@ -30,94 +30,101 @@ public class AddNRandomVertices implements GraphOperation {
 
 	    //TODO: (group assignment) modify below to get the code run faster
         long startTime = System.nanoTime(); // Start the total timing
-		
-        double width = 800; // todo: relate these to actual window space dimensions
-    	double height = 670; // or, in the very least to GraphViewer.windowWidth and Height
-//        ArrayList<Triangle> triangulation = new ArrayList<Triangle>(); //empty triangle mesh
-//        
-//        //add the triangle that surrounds all of the points aka the "super triangle".
-//        Triangle superTriangle = new Triangle (new Vertex("sv1", 0, 0 ), new Vertex("sv2", width + height, 0 ), new Vertex("sv3", 0, width + height ));
-//        triangulation.add(superTriangle);
-//        for (int i=0; i<nRandomVertices; i++) {
-//            double xPos = rand.nextDouble() * width; 
-//            double yPos = rand.nextDouble() * height; 
-//            Vertex v = new Vertex("p"+gmi.getVertices().size(), xPos, yPos);
-//            
-//            //determine triangles to be changed
-//            ArrayList<Triangle> badTriangles = new ArrayList<Triangle>();
-//            for (Triangle triangle: triangulation) {
-//            	if (triangle.pointInsideCircumcircle(v)) {
-//            		badTriangles.add(triangle);
-//            	}
-//            }
-//            
-//            //determine polygon around triangles that need to be changed
-//            ArrayList<Edge> polygon = new ArrayList<Edge>();
-//            for (Triangle badTriangle : badTriangles) { 
-//            	Edge polygonEdge = null;
-//            	outerloop:
-//            	for (Edge edge : badTriangle.getEdges()) {
-//            		for (Triangle otherBadTriangle : badTriangles) {
-//            			if (badTriangle != otherBadTriangle) {
-//            				for (Edge otherEdge: otherBadTriangle.getEdges()) {
-//            					if (edge.equals(otherEdge)) {
-//            						polygonEdge = edge;
-//            						break outerloop;
-//            					}
-//            				}
-//            			}
-//            		}
-//            	}
-//            	polygon.add(polygonEdge);
-//            }
-//            
-//            //remove the outdated triangles
-//            for (Triangle badTriangle : badTriangles) {
-//            	triangulation.remove(badTriangle);
-//            }
-//            
-//            //retriangulate the polygon
-//            for (Edge polyEdge : polygon) {
-//            	triangulation.add(new Triangle(polyEdge, v));
-//            }
-//        }
-//        
-//        //remove the super triangle
-//        for (Edge superEdge : superTriangle.getEdges()) {
-//        	for (Triangle triangle : triangulation) {
-//        		outerLoopTwo:
-//        		for (Edge otherEdge: triangle.getEdges()) {
-//					if (superEdge.equals(otherEdge)) {
-//						triangulation.remove(triangle);
-//						break outerLoopTwo;
-//					}
-//				}
-//        	}
-//        }
-//        
-//        //add triangulation to gmi
-//        for (Triangle triangle : triangulation) {
-//        	for (Edge edge : triangle.getEdges()) {
-//        		Boolean isRepeat = false;
-//        		outerLoopThree:
-//    			for (Edge otherEdge: gmi.getEdges()) {
-//					if (edge.equals(otherEdge)) {
-//						isRepeat = true;
-//						break outerLoopThree;
-//					}
-//				}
-//        		if (isRepeat == false) {
-//        			gmi.getEdges().add(edge);
-//        		}
-//        	}
-//        }
-        
+
+        final double width = 800; // TODO relate these to actual window space dimensions
+    	final double height = 700; // or, in the very least to GraphViewer.windowWidth and Height
+    	/* Delaunay */
+        ArrayList<Triangle> triangulation = new ArrayList<Triangle>(); //empty triangle mesh
+
+        //add the triangle that surrounds all of the points aka the "super triangle".
+        Triangle superTriangle = new Triangle (
+                new Vertex("sv1", -0.1, -0.1 ),           // Right Angle
+                new Vertex("sv2", 2 * width + 0.1, 0 ),
+                new Vertex("sv3", 0, 2 * height + 0.1 ));
+        triangulation.add(superTriangle);
+
+        // create each point as normal
         for (int i=0; i<nRandomVertices; i++) {
-            double xPos = rand.nextDouble() * width; 
-            double yPos = rand.nextDouble() * height; 
+            double xPos = rand.nextDouble() * width;
+            double yPos = rand.nextDouble() * height;
             Vertex v = new Vertex("p"+gmi.getVertices().size(), xPos, yPos);
-            
-            // Edge creation O(n^2). 
+
+            //determine triangles to be changed
+            ArrayList<Triangle> badTriangles = new ArrayList<Triangle>();
+            for (Triangle triangle: triangulation) {
+            	if (triangle.pointInsideCircumcircle(v)) {
+            		badTriangles.add(triangle);
+            	}
+            }
+
+            //determine polygon around triangles that need to be changed
+            ArrayList<Edge> polygon = new ArrayList<Edge>();
+            for (Triangle badTriangle : badTriangles) {
+            	Edge polygonEdge = null;
+            	outerloop:
+            	for (Edge edge : badTriangle.getEdges()) {
+            		for (Triangle otherBadTriangle : badTriangles) {
+            			if (badTriangle != otherBadTriangle) {
+            				for (Edge otherEdge: otherBadTriangle.getEdges()) {
+            					if (edge.equals(otherEdge)) {
+            						polygonEdge = edge;
+            						break outerloop;
+            					}
+            				}
+            			}
+            		}
+            	}
+            	polygon.add(polygonEdge);
+            }
+
+            //remove the outdated triangles
+            for (Triangle badTriangle : badTriangles) {
+            	triangulation.remove(badTriangle);
+            }
+
+            //retriangulate the polygon
+            for (Edge polyEdge : polygon) {
+            	triangulation.add(new Triangle(polyEdge, v));
+            }
+        }
+
+        //remove the super triangle
+        for (Edge superEdge : superTriangle.getEdges()) {
+        	for (Triangle triangle : triangulation) {
+        		outerLoopTwo:
+        		for (Edge otherEdge: triangle.getEdges()) {
+					if (superEdge.equals(otherEdge)) {
+						triangulation.remove(triangle);
+						break outerLoopTwo;
+					}
+				}
+        	}
+        }
+
+        //add triangulation to gmi
+        for (Triangle triangle : triangulation) {
+        	for (Edge edge : triangle.getEdges()) {
+        		Boolean isRepeat = false;
+        		outerLoopThree:
+    			for (Edge otherEdge: gmi.getEdges()) {
+					if (edge.equals(otherEdge)) {
+						isRepeat = true;
+						break outerLoopThree;
+					}
+				}
+        		if (isRepeat == false) {
+        			gmi.getEdges().add(edge);
+        		}
+        	}
+        }
+        /* Delaunay */
+
+        for (int i=0; i<nRandomVertices; i++) {
+            double xPos = rand.nextDouble() * width;
+            double yPos = rand.nextDouble() * height;
+            Vertex v = new Vertex("p"+gmi.getVertices().size(), xPos, yPos);
+
+            // Edge creation O(n^2).
             for (Vertex vB : gmi.getVertices()) {
                 Edge e = new Edge(v, vB);
                 double weight = Math.sqrt(
--- src/d_minSpanTree/model/Triangle.java
+++ src/d_minSpanTree/model/Triangle.java
@@ -3,9 +3,9 @@ package d_minSpanTree.model;
 import java.util.ArrayList;
 
 public class Triangle {
-	
+
 	ArrayList<Edge> edgeArray = new ArrayList<Edge>();
-	
+
 	public Triangle(Vertex vertex, Vertex vertex2, Vertex vertex3) {
 		edgeArray.add(new Edge(vertex, vertex2));
 		edgeArray.add(new Edge(vertex, vertex3));
@@ -21,8 +21,31 @@ public class Triangle {
 	}
 
 	public boolean pointInsideCircumcircle(Vertex v) {
-		// TODO Auto-generated method stub
-		return false;
+	    // The reason for saving so many points is to reduce the number of calculations.
+	    final double Ax = edgeArray.get(0).getStart().getX();
+	    final double Ay = edgeArray.get(0).getStart().getY();
+	    final double Bx = edgeArray.get(2).getStart().getX();
+	    final double By = edgeArray.get(2).getStart().getY();
+	    final double Cx = edgeArray.get(2).getEnd().getX();
+	    final double Cy = edgeArray.get(2).getEnd().getY();
+	    final double Dx = v.getX();
+        final double Dy = v.getY();
+        final double DDx = Dx*Dx;
+        final double DDy = Dy*Dy;
+	    final double a = (Ax - Dx);
+	    final double b = (Ay - Dy);
+	    final double c = (Ax*Ax - DDx) + (Ay*Ay - DDy);
+	    final double d = (Bx - Dx);
+	    final double e = (By - Dy);
+	    final double f = (Bx*Bx - DDx) + (By*By - DDy);
+	    final double g = (Cx - Dx);
+	    final double h = (Cy - Dy);
+	    final double i = (Cx*Cx - DDx) + (Cy*Cy - DDy);
+
+	    // These three lines should be equivalent
+	    // return a*e*i + b*f*g + c*d*h - c*e*g - b*d*i - a*f*h > 0;
+	    // return (a*e*i + b*f*g + c*d*h) - (c*e*g + b*d*i + a*f*h) > 0;
+		return (a*e*i + b*f*g + c*d*h) > (c*e*g + b*d*i + a*f*h);
 	}
 
 	public ArrayList<Edge> getEdges() {
